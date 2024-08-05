053cf28c6765a9940343304fc3eb945d5a0b7388
Thiago <a00s@a00s.com>
2024-08-05 16:57:00 -0300

    tela funcionando sem piscar

--- uv_com_matrix_led.ino
+++ uv_com_matrix_led.ino
@@ -1,20 +1,12 @@
 #include <SoftwareSerial.h>
-// #include "Arduino_LED_Matrix.h"
 #include <Arduino.h> // Include the Arduino library for NVIC_SystemReset()
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
- 
+
 #define TFT_CS        13 // pin CS
 #define TFT_DC         9 // pin Data/Command
 #define TFT_RST        8 // pin Reset, o -1 se collegato al pin reset  Arduino
-
-// ArduinoLEDMatrix matrix;
-
-const int numReadings = 5;
-int readingsA0[numReadings];
-int readingsA1[numReadings];
-int readIndex = 0;
 
 const int rxPin = 6;
 const int txPin = 7;
@@ -24,37 +16,46 @@ SoftwareSerial co2Serial(rxPin, txPin);
 
 int co2Level = 0;
 
-const int numRows = 8;
-const int numCols = 12;
+int32_t uv1 = 0;
+int32_t uv2 = 0;
 
-// Define thresholds for UV and CO2
-const int uvMin = 40;
-const int uvMax = 200;
-const int co2Min = 600;
-const int co2Max = 2000;
-
-// Define minimum and maximum pulse duration and volume
-const int minPulseDuration = 10; // Minimum duration of beep in ms
-const int maxPulseDuration = 500; // Maximum duration of beep in ms
-const int minVolume = 50; // Minimum volume (50% duty cycle)
-const int maxVolume = 255; // Maximum volume (100% duty cycle)
-
-uint8_t frame[numRows][numCols] = {0};
-int maxLevel = 20;
-
-// Counter for consecutive failed CO2 readings
-const int maxFailedReadings = 5;
-int failedReadingsCount = 0;
-// int maxUV = 200;
-
-// Initializza la Adafruit ST7789 TFT library
+// Initialize the Adafruit ST7789 TFT library
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+// Variáveis para controle de tempo e estado
+bool co2ReadingInProgress = false;
+unsigned long co2RequestTime = 0;
+const unsigned long co2Interval = 5000; // Intervalo de tempo entre leituras de CO2 (5 segundos)
+const unsigned long co2Timeout = 10000; // Tempo limite para a leitura de CO2 (10 segundos)
+
+// Limites para o alarme do UV e CO2
+const int uvMin = 300; // Nível mínimo de UV para alarme
+const int uvMax = 800; // Nível máximo de UV para alarme
+const int co2Min = 800; // Nível mínimo de CO2 para alarme
+const int co2Max = 2000; // Nível máximo de CO2 para alarme
+
+// Configuração do buzzer
+const int minPulseDuration = 100; // Duração mínima do pulso do buzzer em ms
+const int maxPulseDuration = 500; // Duração máxima do pulso do buzzer em ms
+const int minVolume = 50; // Volume mínimo do buzzer (0-255)
+const int maxVolume = 255; // Volume máximo do buzzer (0-255)
+const unsigned long alarmInterval = 5000; // Intervalo para o alarme do buzzer em ms
+
+// Variáveis para armazenar os valores antigos
+int lastCo2Level = -1;
+int lastMaxUV = -1;
+
+// Variáveis para controle do estado do buzzer
+bool buzzerActive = false;
+unsigned long buzzerStartTime = 0;
+int currentPulseDuration = 0;
+int currentVolume = 0;
+unsigned long lastAlarmTime = 0;
 
 void setup() {
   Serial.begin(115200);
   pinMode(buzzerPin, OUTPUT);
   co2Serial.begin(9600);
-  // matrix.begin();
 
   // Buzzer test
   Serial.println("Testing buzzer...");
@@ -65,208 +66,160 @@ void setup() {
   analogWrite(buzzerPin, 70); // 50% duty cycle to reduce volume
   delay(40);
   analogWrite(buzzerPin, 0);
-  delay(40);
   analogWrite(buzzerPin, 70); // 50% duty cycle to reduce volume
   delay(40);
   analogWrite(buzzerPin, 0);
 
-
   delay(1000);
   Serial.println("Buzzer test complete.");
 
-  for (int i = 0; i < numReadings; i++) {
-    readingsA0[i] = 0;
-    readingsA1[i] = 0;
-  }
-
-// Inicializa o display com SPI_MODE2
+  // Inicializa o display com SPI_MODE2
   tft.init(240, 240, SPI_MODE2);
   delay(500);  // Adiciona atraso após a inicialização
 
-
-  tft.setRotation(3); //ruota di 180°
- 
+  tft.setRotation(3); // Gira o display 90 graus
   tft.fillScreen(ST77XX_BLACK);
- 
-  // tft.setTextSize(3);   //Imposta la grandezza del testo
-  // tft.setTextColor(ST77XX_WHITE);  //Imposta il colore del testo
-  // tft.setCursor(10, 10);
-  // tft.println("TEST DISPLAY");  //Stringa da visualizzare
- 
-  // tft.setCursor(20, 50);
-  // tft.setTextColor(ST77XX_RED);  //Imposta il colore del testo
-  // tft.println("IPS 240x240");  //Stringa da visualizzare
- 
-  // tft.setCursor(20, 100);
-  // tft.setTextColor(ST77XX_YELLOW);  //Imposta il colore del testo
-  // tft.println("Arduino UNO");  //Stringa da visualizzare
- 
-  // tft.setCursor(20, 150);
-  // tft.setTextSize(2);
-  // tft.setTextColor(ST77XX_GREEN);  //Imposta il colore del testo
-  // tft.println("AEEEEE"); //Stringa da visualizzare
-
 }
 
 void loop() {
-  // Read the current value from the sensor
-  readingsA0[readIndex] = analogRead(A0);
-  readingsA1[readIndex] = analogRead(A1);
-  
-  readIndex = readIndex + 1;
-  
-  if (readIndex >= numReadings) {
-    readIndex = 0;
+  uv1 = analogRead(A0);
+  uv2 = analogRead(A1);
+  int32_t maxUV = max(uv1, uv2);
+
+  // Verifica se é hora de enviar um novo comando de leitura de CO2
+  if (!co2ReadingInProgress && millis() - co2RequestTime >= co2Interval) {
+    requestCO2Reading();
+    co2ReadingInProgress = true;
+    co2RequestTime = millis();
   }
 
-  // Get the maximum value from the UV sensors
-  int maxUV = max(readingsA0[readIndex], readingsA1[readIndex]);
-  
-  // Read CO2 level from MH-Z19B
-  co2Level = readCO2();
-   delay(5000);
-  // Check if CO2 reading failed
-  if (co2Level == -1) {
-    failedReadingsCount++;
-    if (failedReadingsCount >= maxFailedReadings) {
-      Serial.println("Too many consecutive CO2 reading failures, resetting...");
-      NVIC_SystemReset(); // Perform a system reset
+  // Tenta ler a resposta do sensor de CO2 se a leitura estiver em andamento
+  if (co2ReadingInProgress) {
+    int result = readCO2NonBlocking();
+    if (result != -1) {
+      co2Level = result;
+      Serial.print("CO2 Level: ");
+      Serial.println(co2Level);
+      co2ReadingInProgress = false; // Reseta o estado da leitura
     }
-  } else {
-    failedReadingsCount = 0; // Reset the counter if a valid reading is received
+    // Reinicia a leitura de CO2 se não houver resposta em 10 segundos
+    else if (millis() - co2RequestTime >= co2Timeout) {
+      Serial.println("CO2 sensor not responding, resetting...");
+      co2ReadingInProgress = false; // Cancela a leitura em andamento
+      requestCO2Reading(); // Reinicia a leitura
+      co2RequestTime = millis(); // Atualiza o tempo do último pedido
+    }
   }
 
-  // Print the readings and the maximum value
-  Serial.print("A0: ");
-  Serial.print(readingsA0[readIndex]);
-  Serial.print("\tA1: ");
-  Serial.print(readingsA1[readIndex]);
-  Serial.print("\tMax UV: ");
-  Serial.print(maxUV);
-  Serial.print("\tCO2: ");
-  Serial.println(co2Level);
- 
+  // Atualiza o display com as leituras atuais
   updateDisplay(maxUV, co2Level);
- 
-  // Define the number of LEDs to light directly from the UV sensor reading
-  // int ledsToLightUV = map((long)maxUV, 0, (long)maxLevel, 0, (long)(numRows * (numCols / 2)));
-  int ledsToLightUV = map((long)maxUV, 0, (long)maxLevel, 0, (long)(numRows * (numCols / 2)));
-  // Define the number of LEDs to light directly from the CO2 sensor reading
-  int ledsToLightCO2 = map((long)co2Level, 0, 2000, 0, (long)(numRows * (numCols / 2)));
-  
-  // Update the frame of the LED matrix based on the sensor readings
-  // updateLedMatrix(ledsToLightUV, ledsToLightCO2);
 
-  // Render the frame on the LED matrix
-  // matrix.renderBitmap(frame, numRows, numCols);
-
-  // Check if thresholds are exceeded and activate the buzzer with pulsing frequency
+  // Controla o buzzer baseado nos níveis de UV e CO2
   controlBuzzer(maxUV, co2Level);
 
-  // Wait 2 seconds before taking the next reading
-  delay(2000);
+  // Print the readings and the maximum value
+  // Serial.print("A0: ");
+  // Serial.print(uv1);
+  // Serial.print("\tA1: ");
+  // Serial.print(uv2);
+  // Serial.print("\tMax UV: ");
+  // Serial.println(maxUV);
+
+  // Delay para permitir a leitura do próximo ciclo sem bloqueio
+  delay(20);
 }
 
-void controlBuzzer(int maxUV, int co2Level) {
-  bool uvAlert = maxUV > uvMin;
-  bool co2Alert = co2Level > co2Min;
-
-  if (uvAlert || co2Alert) {
-    int uvPulseDuration = uvAlert ? map(maxUV, uvMin, uvMax, minPulseDuration, maxPulseDuration) : 0;
-    int co2PulseDuration = co2Alert ? map(co2Level, co2Min, co2Max, minPulseDuration, maxPulseDuration) : 0;
-    int pulseDuration = max(uvPulseDuration, co2PulseDuration);
-
-    int uvVolume = uvAlert ? map(maxUV, uvMin, uvMax, minVolume, maxVolume) : 0;
-    int co2Volume = co2Alert ? map(co2Level, co2Min, co2Max, minVolume, maxVolume) : 0;
-    int volume = max(uvVolume, co2Volume);
-
-    Serial.print("Pulse Duration: ");
-    Serial.print(pulseDuration);
-    Serial.print(" ms, Volume: ");
-    Serial.println(volume);
-    analogWrite(buzzerPin, volume); // Set volume
-    delay(pulseDuration); // Duration of beep
-    analogWrite(buzzerPin, 0);
-    delay(100); // Delay between beeps
-  } else {
-    analogWrite(buzzerPin, 0); // Ensure the buzzer is off
-  }
+void requestCO2Reading() {
+  // Envia o comando para solicitar a leitura de CO2
+  co2Serial.write((uint8_t)0xFF);
+  co2Serial.write((uint8_t)0x01);
+  co2Serial.write((uint8_t)0x86);
+  co2Serial.write((uint8_t)0x00);
+  co2Serial.write((uint8_t)0x00);
+  co2Serial.write((uint8_t)0x00);
+  co2Serial.write((uint8_t)0x00);
+  co2Serial.write((uint8_t)0x00);
+  co2Serial.write((uint8_t)0x79);
 }
 
-// void updateLedMatrix(int ledsToLightUV, int ledsToLightCO2) {
-//   memset(frame, 0, sizeof(frame));
-
-//   int count = 0;
-//   for (int row = 0; row < numRows; row++) {
-//     for (int col = 0; col < numCols / 2; col++) {
-//       if (count < ledsToLightUV) {
-//         frame[row][col] = 1;
-//         count++;
-//       } else {
-//         frame[row][col] = 0;
-//       }
-//     }
-//   }
-
-//   count = 0;
-//   for (int row = 0; row < numRows; row++) {
-//     for (int col = numCols / 2; col < numCols; col++) {
-//       if (count < ledsToLightCO2) {
-//         frame[row][col] = 1;
-//         count++;
-//       } else {
-//         frame[row][col] = 0;
-//       }
-//     }
-//   }
-// }
-
-int readCO2() {
-  byte response[9];
-  const int maxRetries = 5;
-  int retries = 0;
-  int co2Value = -1;
-
-  while (retries < maxRetries && co2Value == -1) {
-    co2Serial.write((uint8_t)0xFF);
-    co2Serial.write((uint8_t)0x01);
-    co2Serial.write((uint8_t)0x86);
-    co2Serial.write((uint8_t)0x00);
-    co2Serial.write((uint8_t)0x00);
-    co2Serial.write((uint8_t)0x00);
-    co2Serial.write((uint8_t)0x00);
-    co2Serial.write((uint8_t)0x00);
-    co2Serial.write((uint8_t)0x79);
-
-    memset(response, 0, 9);
+int readCO2NonBlocking() {
+  // Verifica se há dados disponíveis para leitura
+  if (co2Serial.available() >= 9) {
+    byte response[9];
     co2Serial.readBytes(response, 9);
 
     if (response[0] == 0xFF && response[1] == 0x86) {
-      co2Value = (response[2] << 8) + response[3];
-    } else {
-      retries++;
-      delay(100);
+      return (response[2] << 8) + response[3];
     }
   }
-
-  return co2Value;
+  return -1; // Retorna -1 se a leitura não estiver completa ou válida
 }
 
 void updateDisplay(int maxUV, int co2Level) {
-  tft.setTextSize(9);
-  // Clear the area where CO2 level is displayed
-  tft.fillRect(0, 0, 240, 240, ST77XX_BLACK); // Adjust width and height as needed
+  tft.setTextSize(9); // Ajusta o tamanho do texto
+  tft.setFont(); // Usa a fonte padrão do Adafruit GFX
 
-  // Set text color and display new CO2 level
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setCursor(10, 10);
-  tft.println(co2Level);
+  // Atualiza a exibição do nível de CO2 somente se mudou
+  if (co2Level != lastCo2Level) {
+    tft.setCursor(5, 30); // Ajusta a posição para centralizar verticalmente
+    tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK); // Define a cor do texto e o fundo preto
+    tft.print(lastCo2Level); // Apaga o número antigo desenhando o fundo
+    tft.setCursor(5, 30); // Reajusta o cursor após apagar
+    tft.print(co2Level); // Desenha o novo número
+    lastCo2Level = co2Level; // Atualiza o valor antigo de CO2
+  }
 
-  // Clear the area where UV level is displayed
-  // tft.fillRect(10, 70, 200, 30, ST77XX_BLACK); // Adjust width and height as needed
+  // Atualiza a exibição do nível de UV somente se mudou
+  if (maxUV != lastMaxUV) {
+    tft.setCursor(5, 150); // Ajusta a posição para centralizar verticalmente
+    tft.setTextColor(ST77XX_RED, ST77XX_BLACK); // Define a cor do texto e o fundo preto
 
-  // Set text color and display new UV level
-  tft.setTextColor(ST77XX_RED);
-  tft.setCursor(10, 100);
-  tft.println(maxUV);
+    // Converte maxUV em uma string com 4 caracteres
+    char uvString[5]; // Cria um buffer para a string
+    snprintf(uvString, sizeof(uvString), "%-4d", maxUV); // Formata maxUV para ser uma string de 4 caracteres
+
+    tft.print(lastMaxUV); // Apaga o número antigo desenhando o fundo
+    tft.setCursor(5, 150); // Reajusta o cursor após apagar
+    tft.print(uvString); // Desenha o novo número formatado
+    lastMaxUV = maxUV; // Atualiza o valor antigo de UV
+  }
+}
+
+
+
+void controlBuzzer(int maxUV, int co2Level) {
+  // Verifica se os níveis de UV ou CO2 estão acima dos limites mínimos
+  bool uvAlert = maxUV > uvMin;
+  bool co2Alert = co2Level > co2Min;
+
+  if ((uvAlert || co2Alert) && millis() - lastAlarmTime >= alarmInterval) {
+    // Calcula a duração do pulso com base nos níveis de UV e CO2
+    int uvPulseDuration = uvAlert ? map(maxUV, uvMin, uvMax, minPulseDuration, maxPulseDuration) : 0;
+    int co2PulseDuration = co2Alert ? map(co2Level, co2Min, co2Max, minPulseDuration, maxPulseDuration) : 0;
+    currentPulseDuration = max(uvPulseDuration, co2PulseDuration);
+
+    // Calcula o volume do buzzer com base nos níveis de UV e CO2
+    int uvVolume = uvAlert ? map(maxUV, uvMin, uvMax, minVolume, maxVolume) : 0;
+    int co2Volume = co2Alert ? map(co2Level, co2Min, co2Max, minVolume, maxVolume) : 0;
+    currentVolume = max(uvVolume, co2Volume);
+
+    if (!buzzerActive) {
+      // Inicia o alarme
+      buzzerStartTime = millis();
+      analogWrite(buzzerPin, currentVolume); // Define o volume do buzzer
+      buzzerActive = true;
+      lastAlarmTime = millis(); // Atualiza o tempo do último alarme
+      // Serial.print("Activating Buzzer - Duration: ");
+      // Serial.print(currentPulseDuration);
+      // Serial.print(" ms, Volume: ");
+      // Serial.println(currentVolume);
+    } else if (millis() - buzzerStartTime >= currentPulseDuration) {
+      // Desliga o buzzer após a duração do pulso
+      analogWrite(buzzerPin, 0);
+      buzzerActive = false;
+    }
+  } else {
+    analogWrite(buzzerPin, 0); // Garante que o buzzer está desligado
+    buzzerActive = false;
+  }
 }
